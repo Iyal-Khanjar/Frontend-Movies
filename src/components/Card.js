a17bbefb2d73e27157aed4ae26c7031431659d5d
Iyal Khanjar <ayalkhanjar@gmail.com>
2022-02-09 09:30:57 +0200

    add star to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ export default function Card({ data,isMovie=true,urlLink,type}) {
     <div className="movie">
 
         <Link to={`/${type}/${data.id}`} className="flip-card-front">
-          <div className="card-movie-header-chip vote-avg">{data.vote_average}</div>
+          <div className="card-movie-header-chip vote-avg"><i class="fas fa-star"></i> {data.vote_average}</div>
           <div className="card-movie-header-chip movie-year">{releaseYear}</div>
           <img className="card-movie-img" src={data.poster_path ? urlLink + data.poster_path : 'https://static.bond.edu.au/sites/default/files/styles/full_width/public/cinema%20750x320.jpg?itok=U8R3z3ov'} alt={data.title} />
           <div className="movie-info">
